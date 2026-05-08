23a0735a212397bcba4272eca3ca7b3c66d09874
pgsnSaisadhan <skodurupaka@pgsnsofttech.com>
2026-05-08 16:23:54 +0530

    Initial clean DevOps CI/CD project

---
+++ main.tf
@@ -0,0 +1,149 @@
+# =========================
+# RESOURCE GROUP
+# =========================
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# =========================
+# VIRTUAL NETWORK
+# =========================
+resource "azurerm_virtual_network" "vnet" {
+  name                = "saisadhan-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# =========================
+# SUBNET
+# =========================
+resource "azurerm_subnet" "subnet" {
+  name                 = "saisadhan-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# =========================
+# PUBLIC IP
+# =========================
+resource "azurerm_public_ip" "publicip" {
+  name                = "saisadhan-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# =========================
+# NSG (OPEN 80, 22 OPTIONAL)
+# =========================
+resource "azurerm_network_security_group" "nsg" {
+  name                = "saisadhan-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# =========================
+# NIC
+# =========================
+resource "azurerm_network_interface" "nic" {
+  name                = "saisadhan-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# =========================
+# NSG ATTACHMENT
+# =========================
+resource "azurerm_network_interface_security_group_association" "nsgassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# =========================
+# LINUX VM (PASSWORD LOGIN)
+# =========================
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+set -e
+
+apt-get update -y
+apt-get install -y docker.io
+
+systemctl enable docker
+systemctl start docker
+
+docker rm -f webapp || true
+
+docker pull ${var.docker_username}/devops-app:latest
+
+docker run -d \
+  --restart unless-stopped \
+  --name webapp \
+  -p 80:80 \
+  ${var.docker_username}/devops-app:latest
+
+EOF
+)
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "saisadhan-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "vm_name" {
+  description = "VM name"
+  type        = string
+  default     = "docker-vm"
+}
+
+variable "admin_username" {
+  description = "VM admin username"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "VM admin password"
+  type        = string
+  sensitive   = true
+}
+variable "my_ip" {
+  description = "Your public IP for SSH access (use /32 format)"
+  type        = string
+  default     = "49.37.120.15/32"
+}
+variable "docker_username" {
+  type = string
+}
